0f668d62b4e8c631697f86e9ece9caff25d66d34
chiara.bitschnau@student.htl-rankweil.at <chiara.bitschnau@student.htl-rankweil.at>
2026-03-18 10:37:34 +0000

    Edited Startseite.Verkaufsuebersicht

--- client_code/Startseite/Verkaufsuebersicht/__init__.py
+++ client_code/Startseite/Verkaufsuebersicht/__init__.py
@@ -18,3 +18,53 @@ class Verkaufsuebersicht(VerkaufsuebersichtTemplate):
   def button_startseite_von_verkaufsuebersicht_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form("Startseite", row_dict=self.item)
+
+
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+    self.plot_1.data = [{
+      "labels": ["Äpfel", "Bananen", "Kirschen"],
+      "values": [10, 20, 30],
+      "type": "pie"
+    }]
+
+  def __init__(self, **properties):
+    self.init_components(**properties)
+
+    # 🔝 Großer Haupttitel (Label im Designer: label_1)
+    self.label_1.text = "Die meisten Verkäufe"
+
+    # 🥧 Diagramm 1 (links)
+    self.plot_1.data = [{
+      "labels": ["Äpfel", "Bananen", "Kirschen"],
+      "values": [10, 20, 30],
+      "type": "pie",
+      "textinfo": "label+percent"
+    }]
+
+    self.plot_1.layout = {
+      "title": {
+        "text": "nach Stück",
+        "x": 0.5,
+        "xanchor": "center",
+        "font": {"size": 18}
+      }
+    }
+
+    # 🥧 Diagramm 2 (rechts)
+    self.plot_2.data = [{
+      "labels": ["Rot", "Blau", "Grün"],
+      "values": [15, 25, 10],
+      "type": "pie",
+      "textinfo": "label+percent"
+    }]
+
+    self.plot_2.layout = {
+      "title": {
+        "text": "nach Umsatz in €",
+        "x": 0.5,
+        "xanchor": "center",
+        "font": {"size": 18}
+      }
+    }
